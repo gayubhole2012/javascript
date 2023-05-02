3caf6ceb9ca999d84a8c45d5f2ecc8dbea988c0f
Gayatri <gayatri@admins-MacBook-Pro.local>
2023-05-02 14:17:40 -0700

    ineraction alert,prompt and confirm

---
+++ interaction.js
@@ -0,0 +1,15 @@
+//alert
+alert("hello guys");
+
+//prompt- prompt accepts two argument(title, [default]);
+let age = prompt('how old are you?',100 );
+alert(`you are ${age} years old`);
+
+//confirm - confirm shows model window
+let isBoss = confirm("are you the boss?");
+alert(isBoss);//true if ok  is pressed
+
+
+//task
+let name2= prompt('what is your name?','Jonny');
+alert(`your name is ${name2}`);

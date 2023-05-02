a2f082ce6a7f7de5ab79d11b56c22f915f7187af
Gayatri <gayatri@admins-MacBook-Pro.local>
2023-05-01 18:55:40 -0700

    variable

---
+++ variable.js
@@ -0,0 +1,10 @@
+//let – is a modern variable declaration.
+//var – is an old-school variable declaration. Normally we don’t use it at all, but we’ll cover subtle differences from let in the chapter The old "var", 
+//just in case you need them.
+//const – is like let, but the value of the variable can’t be changed.
+//using let keywword varibale
+
+let admin,name;
+name= 'Jhon';
+admin = name;
+alert(admin);//Jhon

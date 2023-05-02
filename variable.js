858e8442ef7f27375ee94cd4a75036633d027433
Gayatri <gayatri@admins-MacBook-Pro.local>
2023-05-01 21:06:53 -0700

    some more example var and let keyword

--- variable.js
+++ variable.js
@@ -1,10 +1,24 @@
 //let – is a modern variable declaration.
-//var – is an old-school variable declaration. Normally we don’t use it at all, but we’ll cover subtle differences from let in the chapter The old "var", 
-//just in case you need them.
+//var - 
 //const – is like let, but the value of the variable can’t be changed.
 //using let keywword varibale
 
-let admin,name;
-name= 'Jhon';
-admin = name;
-alert(admin);//Jhon
+//let admin,name;
+//name= 'Jhon';
+//admin = name;
+//alert(admin);//Jhon
+
+// var example
+myName= 'Jenet';
+function logName(){
+    console.log(myName);
+}
+logName();
+var myName;//Jenet  
+//above example is work because of hoisting all varibles goes on top 
+
+var myChar= "Jonny";
+var myChar = "Biba"; //its works in var keyword you can declare many times same variable
+
+let myAgent = "Monica";
+let myAgent = "Robbin"; // its gives error because let keyword its morden concept introduce 2015

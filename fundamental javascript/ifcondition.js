684f72c3aa8d855400a77e9a6e2b232d663dbe76
Gayatri <gayatri@admins-MacBook-Pro.local>
2023-05-09 13:16:34 -0700

    fundamental javascript

---
+++ fundamental javascript/ifcondition.js
@@ -0,0 +1,23 @@
+//task
+if("0"){
+    console.log('hello');//hello
+}
+
+/*let value = prompt("Type a Number",0);
+if(value > 0){
+ alert(1);
+}else if(value < 0){
+    alert(-1)
+}else{
+    alert(0);
+}*/
+
+let result;
+if(a+b < 4){
+    result = 'Below';
+    }else{
+        result = 'Over';
+    }
+
+//we can write same above code using ternary operators
+let result1 = (a + b < 4)? 'Below' : 'Over';
